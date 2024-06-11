391210e45e6578efd31f2e48ca6ac50b73abbe20
subina <subinaghimire@gmail.com>
2024-06-11 20:25:43 +1000

    switched tasks between entry and bad list

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const obj= {
     task,
     hr,
     id:randomIdGenerator(),
+    type:"entry",
 };
 
   taskList.push(obj);
@@ -21,20 +22,45 @@ const displayEntryList = () => {
     let str= "";
 
     const entryElm = document.getElementById("entryList");
-    taskList.map((item, i) =>{
+
+    const entryList = taskList.filter((item) => item.type === 'entry' );
+
+    entryList.map((item, i) =>{
      str+= `<tr>
     <td>${i + 1}</td>
     <td>${item.task}</td>
     <td>${item.hr}</td>
     <td class="text-end">
         <button onclick="handleOnDelete('${item.id}')" class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
-        <button class="btn btn-success"><i class="fa-solid fa-arrow-right"></i></button>
+        <button onclick="switchTask('${item.id}', 'bad')" class="btn btn-success"><i class="fa-solid fa-arrow-right"></i></button>
     </td>
     </tr>`   
     })
     entryElm.innerHTML =str;
 }
 
+const displayBadList = () => {
+   
+    let str= "";
+
+    const badElm = document.getElementById("badList");
+
+    const badList = taskList.filter((item) => item.type ==="bad" );
+
+   badList.map((item, i) =>{
+     str+= ` <tr>
+                <td>${i + 1}</td>
+                <td>${item.task}</td>
+                <td>${item.hr}</td>
+                <td class="text-end">
+                    
+                    <button  onclick="switchTask('${item.id}', 'entry')" class="btn btn-warning"><i class="fa-solid fa-arrow-left"></i></button>
+                    <button onclick="handleOnDelete('${item.id}')"  class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+                </td>
+              </tr>`   
+    })
+    badElm.innerHTML =str;
+}
 
 const randomIdGenerator = (length = 6) =>{
 const str = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
@@ -53,6 +79,18 @@ const handleOnDelete = (id) => {
     if (window.confirm("Are you sure, you want to delete this?")){
         taskList = taskList.filter((item) => item.id!== id);
         displayEntryList();
+        displayBadList();
     }
    
+};
+
+const switchTask =(id, type)=>{
+taskList = taskList.map((item) =>{
+    if(item.id === id) {
+        item.type = type;
+    }
+return item;
+});
+displayEntryList();
+displayBadList();
 };
