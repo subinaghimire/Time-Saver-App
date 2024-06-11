b44afd5cf4faf8773141374519b991175f10b4c5
subina <subinaghimire@gmail.com>
2024-06-11 15:11:56 +1000

    stored form data in state variables

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+let taskList = [];
+
 const handleOnSubmit = (e) =>{
 // const elm = document.getElementById("task");
 const newForm = new FormData(e);
 
 const task = newForm.get("task");
 const hr = newForm.get("hr");
+const obj= {
+    task,
+    hr,
+};
 
-    console.log(task, hr);
+  taskList.push(obj);
+    console.log(taskList);
 }
