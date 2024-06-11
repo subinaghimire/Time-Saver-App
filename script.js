941bb26a35413b299315e43ccced512bf4f174d1
subina <subinaghimire@gmail.com>
2024-06-11 15:33:39 +1000

    displayed data in the table of webpage

--- script.js
+++ script.js
@@ -12,5 +12,24 @@ const obj= {
 };
 
   taskList.push(obj);
+    displayEntryList();
+}
+
+const displayEntryList = () => {
     console.log(taskList);
+    let str= "";
+
+    const entryElm = document.getElementById("entryList");
+    taskList.map((item, i) =>{
+     str+= `<tr>
+    <td>${i + 1}</td>
+    <td>${item.task}</td>
+    <td>${item.hr}</td>
+    <td class="text-end">
+        <button class="btn btn-danger"><i class="fa-solid fa-trash-can"></i></button>
+        <button class="btn btn-success"><i class="fa-solid fa-arrow-right"></i></button>
+    </td>
+    </tr>`   
+    })
+    entryElm.innerHTML =str;
 }
